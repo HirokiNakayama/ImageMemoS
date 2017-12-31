8ef5e4d420d5f59697443afa169bf130fc66c1c2
HirokiNakayama <30726963+HirokiNakayama@users.noreply.github.com>
2017-12-31 21:52:53 +0900

    [add]動画再生画面追加

--- ImageMemoS/ViewController/PlayerViewController.swift
+++ ImageMemoS/ViewController/PlayerViewController.swift
@@ -7,18 +7,39 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
+import Photos
 
-class PlayerViewController: UIViewController {
+class PlayerViewController: AVPlayerViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // 再生動画取得
+        let assets = PHAsset.fetchAssets(in: PhotoCollection.getCorrection(), options: nil)
+        let asset = assets.object(at: PhotoCollection.getSelectNum())
+        
+        // 動画パスの要求
+        let options = PHVideoRequestOptions()
+        options.isNetworkAccessAllowed = true
+        PHImageManager.default().requestAVAsset(forVideo: asset, options: options, resultHandler: {
+            (avAsset, audioMix, info) in
+            
+            if let asset = avAsset {
+                // メインスレッドで再生開始
+                DispatchQueue.global().async {
+                    DispatchQueue.main.async {
+                        self.player = AVPlayer(playerItem: AVPlayerItem(asset: asset))
+                        self.player!.play()
+                    }
+                }
+            }
+        })
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 }
